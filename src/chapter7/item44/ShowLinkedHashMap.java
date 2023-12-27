1ed970067d613d9e751126ca6e0705ab4357806d
lichee55 <lichee55@gmail.com>
2023-12-27 22:45:25 +0900

    item44 complete

---
+++ src/chapter7/item44/ShowLinkedHashMap.java
@@ -0,0 +1,11 @@
+package chapter7.item44;
+
+import java.util.LinkedHashMap;
+
+public class ShowLinkedHashMap {
+	public static void main(String[] args) {
+		LinkedHashMap<?, ?> map = new LinkedHashMap<>();
+
+//		java.util.function.
+	}
+}
