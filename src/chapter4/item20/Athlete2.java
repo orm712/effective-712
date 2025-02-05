dc7795dfad07de47a209614aa2c3b7f4302c6353
xkakr <xkakrlfh@naver.com>
2025-02-05 21:39:15 +0900

    fix : abstract class implements

--- src/chapter4/item20/Athlete2.java
+++ src/chapter4/item20/Athlete2.java
@@ -26,14 +26,15 @@ abstract class BallSportsAthlete implements Athlete2{
     }
 }
 
-class SoccerPlayer2 extends BallSportsAthlete implements Athlete2{
+//속 계층에서 인터페이스 구현이 자동으로 전달되기 때문에, 이미 상위 클래스인 BallSportsAthlete가 Athlete2를 구현하고 있다면, 하위 클래스인 SoccerPlayer2에서 다시 명시적으로 implements Athlete2를 선언할 필요가 없습니다.
+class SoccerPlayer2 extends BallSportsAthlete{
     @Override
     public void 연습() {
         System.out.println("드리블 연습");
     }
 }
 
-class BasketBallPlayer2 extends BallSportsAthlete implements Athlete2{
+class BasketBallPlayer2 extends BallSportsAthlete{
     @Override
     public void 연습() {
         System.out.println("자유투 연습");
